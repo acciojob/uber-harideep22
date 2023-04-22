c6c5caf0121c10f7bd98df063d16eae0885f6855
Harideep KS <harideepkshyam2@gmail.com>
2023-04-22 18:54:40 +0530

    1st try

---
+++ src/main/java/com/driver/model/Admin.java
@@ -0,0 +1,50 @@
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+    public Admin(int adminId, String userName, String password) {
+        this.adminId = adminId;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public Admin() {
+
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int adminId;
+    private String userName;
+    private String password;
+
+
+
+}
---
+++ src/main/java/com/driver/model/Driver.java
@@ -0,0 +1,60 @@
+package com.driver.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Driver{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int driverId;
+    private String mobile;
+    private String password;
+    @OneToOne(mappedBy = "driver",cascade = CascadeType.ALL)
+    Cab cab;
+
+    @OneToMany(mappedBy = "driver",cascade = CascadeType.ALL)
+    List<TripBooking> tripBookingList=new ArrayList<>();
+
+    public Driver() {
+    }
+
+    public Cab getCab() {
+        return cab;
+    }
+
+    public void setCab(Cab cab) {
+        this.cab = cab;
+    }
+
+    public Driver(int driverId, String mobile, String password, Cab cab) {
+        this.driverId = driverId;
+        this.mobile = mobile;
+        this.password = password;
+        this.cab = cab;
+    }
+
+    public int getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(int driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
